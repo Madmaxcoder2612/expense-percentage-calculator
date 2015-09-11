16125ca2053916706b4b4776eab1f2f1b5810893
Zarcaxm <andrebloggergaming@gmail.com>
2015-09-11 03:21:49 +0100

    A simple calculator that calculates the percentage of your salary that you spend on your expenses

---
+++ expensecalculator.py
@@ -0,0 +1,11 @@
+__author__ = 'Zarcaxm'
+
+
+
+
+salary = input ("Insert the value of your salary ")
+expenses = input ("insert the value of your expenses ")
+
+percentage = (float(expenses)*100)/float(salary)
+
+print("You spend ",percentage ,"%", "of your salary in your expenses.")
